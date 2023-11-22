1b166c03e318fdbab8c6d19b6446aff6c75c55d7
sunweihao97 <sunweihao97@gmail.com>
2023-11-22 10:23:42 +0000

    201122 update

--- module/module_sun.py
+++ module/module_sun.py
@@ -16,3 +16,35 @@ def mask_use_shapefile(ncfile, latname, lonname, shp):
     clipped    = ncfile.rio.clip(shape_file.geometry.apply(mapping), shape_file.crs, drop=False)
 
     return clipped
+
+def set_cartopy_tick(ax, extent, xticks, yticks, nx=0, ny=0,
+    xformatter=None, yformatter=None,labelsize=20):
+    import cartopy.crs as ccrs
+    import matplotlib.ticker as mticker
+    from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+    # 本函数设置地图上的刻度 + 地图的范围
+    proj = ccrs.PlateCarree()
+    ax.set_xticks(xticks, crs=proj)
+    ax.set_yticks(yticks, crs=proj)
+    # 设置次刻度.
+    xlocator = mticker.AutoMinorLocator(nx + 1)
+    ylocator = mticker.AutoMinorLocator(ny + 1)
+    ax.xaxis.set_minor_locator(xlocator)
+    ax.yaxis.set_minor_locator(ylocator)
+
+    # 设置Formatter.
+    if xformatter is None:
+        xformatter = LongitudeFormatter()
+    if yformatter is None:
+        yformatter = LatitudeFormatter()
+    ax.xaxis.set_major_formatter(xformatter)
+    ax.yaxis.set_major_formatter(yformatter)
+
+    # 设置axi label_size，这里默认为两个轴
+    ax.tick_params(axis='both',labelsize=labelsize)
+
+    # 在最后调用set_extent,防止刻度拓宽显示范围.
+    if extent is None:
+        ax.set_global()
+    else:
+        ax.set_extent(extent, crs=proj)
